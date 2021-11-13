5c1d47a0860f62ea5978a0123f9d383432fe2eea
Benno Stein <benno.stein@gmail.com>
2021-11-12 22:50:38 -0700

    add support for multiple jar files

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # Generate an application-only callgraph for a Java program using WALA.
-# run as: run.py <entry_class> <jarfile> <output>
+# run as: run.py <entry_class> <output> <jarfile_1> <jarfile_2> ... <jarfile_n>
 # in order to:
 #  (1) analyze <jarfile> with WALA from all public methods in <entry_class>
 #  (2) write to <output> a text file containing all application-code-to-application-code callgraph edges
@@ -11,11 +11,13 @@ import subprocess
 import sys
 
 entry_class = sys.argv[1]
-jarfile = sys.argv[2]
-output_path = sys.argv[3]
+output_path = sys.argv[2]
+jarfiles = sys.argv[3:]
 
 with open("scopefile.tmp", "w") as scopefile:
-    scopefile.write("Primordial,Java,stdlib,none\n\Primordial,Java,jarFile,primordial.jar.model\nApplication,java,jarFile,%s" % jarfile)
+    scopefile.write("Primordial,Java,stdlib,none\n\Primordial,Java,jarFile,primordial.jar.model\n")
+    for jarfile in jarfiles:
+        scopefile.write("Application,java,jarFile,%s\n" % jarfile)
 
 gradle_cmd = './gradlew run --args="-entryClass %s -scopeFile scopefile.tmp -out %s"' % (entry_class, output_path)
 
